14a4be9b3bb64accbf605cd695afdac428c0dbea
ericliu0000 <55063842+ericliu0000@users.noreply.github.com>
2020-08-18 14:53:02 -0400

    Update assignments
    Wording clarifications in assignments 3 and 4

--- Unit 1/Assignment_1.3.py
+++ Unit 1/Assignment_1.3.py
@@ -3,7 +3,7 @@ Middle Grade Stem - Python
 Assignment 1.3
 
 INSTRUCTIONS:
-Comment "Hello World!" on one line using hashtag
+Comment "Hello World!" on one line in inline quotes (using hashtags)
 
 EXAMPLE OUTPUT:
 [this section intentionally left blank]
--- Unit 1/Assignment_1.4.py
+++ Unit 1/Assignment_1.4.py
@@ -3,7 +3,7 @@ Middle Grade Stem - Python
 Assignment 1.4
 
 INSTRUCTIONS:
-Comment "Hello World!" on two separate lines using commas
+Comment "Hello World!" on two separate lines in block quotes (using double quotes)
 
 EXAMPLE OUTPUT:
 [this section intentionally left blank]
